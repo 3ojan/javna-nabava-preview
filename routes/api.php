683d7da41b50ae7dfaf0805e928daa99644f46c8
bojan ratkovic <bojan.ratkovic@speck.agency>
2023-10-13 10:04:56 +0200

    created image uplad controller and model

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+///images
+//For adding an image
+Route::get('/add-image',[App\Http\Controllers\ImageUploadController::class,'addImage'])->name('images.add');
+//For storing an image
+Route::post('/store-image',[App\Http\Controllers\ImageUploadController::class,'storeImage']);
+///images
